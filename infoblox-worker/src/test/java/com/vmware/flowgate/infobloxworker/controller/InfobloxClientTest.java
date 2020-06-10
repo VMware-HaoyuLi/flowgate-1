ed63a3c4ab02f8c5661e05d3a24280f58a112e52
haoyul <haoyul@vmware.com>
2020-06-10 21:07:12 +0800

    debug 5
    Signed-off-by: haoyul <haoyul@vmware.com>

--- infoblox-worker/src/test/java/com/vmware/flowgate/infobloxworker/controller/InfobloxClientTest.java
+++ infoblox-worker/src/test/java/com/vmware/flowgate/infobloxworker/controller/InfobloxClientTest.java
@@ -34,48 +34,5 @@ public class InfobloxClientTest {
     @Mock
     private JsonResultForQueryHostNames jsonHostNameResult = new JsonResultForQueryHostNames();
 
-    @Test
-    public void queryHostNamesByIPTest() {
-        String ip = "192.168.1.6";
-        List<String> successResult = new ArrayList<String>();
-        successResult.add("abc");
-        successResult.add("bcd");
-        successResult.add("def");
-        initJsonHostNameResult();
-        InfobloxClient client = new InfobloxClient(initInfobloxData());
-        client = Mockito.spy(client);
-        Mockito.doReturn(jsonHostNameResult).when(client).getHostNameList(ip);
-        List<String> ret = client.queryHostNamesByIP(ip);
-        TestCase.assertEquals(successResult, ret);
-    }
 
-    private FacilitySoftwareConfig initInfobloxData() {
-        FacilitySoftwareConfig infoBlox = new FacilitySoftwareConfig();
-        infoBlox.setName("Nlyte");
-        infoBlox.setUserName("admin");
-        infoBlox.setPassword("Admin!23");
-        infoBlox.setServerURL("10.160.46.136");
-        infoBlox.setUserId("1");
-        infoBlox.setVerifyCert(false);
-        infoBlox.setType(SoftwareType.InfoBlox);
-    return infoBlox;
-    }
-    private void initJsonHostNameResult() {
-        List<Infoblox> result = new ArrayList<Infoblox>();
-        Infoblox infoblox = new Infoblox();
-        String[] hostnames = {"abc", "bcd.com", "def.com"};
-        infoblox.set_ref("123");
-        infoblox.setHostNames(hostnames);
-        infoblox.setIpAddress("192.168.1.6");
-        infoblox.setIsConflict(true);
-        infoblox.setMacAddress("123");
-        infoblox.setNetwork("123");
-        infoblox.setNetworkView("123");
-        infoblox.setObjects(hostnames);
-        infoblox.setStatus("123");
-        infoblox.setTypes(hostnames);
-        infoblox.setUsage(hostnames);
-        result.add(infoblox);
-        jsonHostNameResult.setResult(result);
-    }
 }
