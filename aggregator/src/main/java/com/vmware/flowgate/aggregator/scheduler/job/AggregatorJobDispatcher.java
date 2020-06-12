7537e0ed63e22d629113bc3f187f5ae65c2d5351
haoyul <haoyul@vmware.com>
2020-06-12 10:26:04 +0800

    test
    Signed-off-by: haoyul <haoyul@vmware.com>

--- aggregator/src/main/java/com/vmware/flowgate/aggregator/scheduler/job/AggregatorJobDispatcher.java
+++ aggregator/src/main/java/com/vmware/flowgate/aggregator/scheduler/job/AggregatorJobDispatcher.java
@@ -99,5 +99,4 @@ public class AggregatorJobDispatcher extends BaseJob implements Job {
       }
       template.opsForValue().set(EventMessageUtil.AGGREGATOR_EXECOUNT, String.valueOf(execount));
    }
-
 }
